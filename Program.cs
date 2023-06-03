2195913445084e537dbbff71794dfb12d392ea3f
Breno Luca <brenoluca046@gmail.com>
2023-06-02 22:41:44 -0300

    Tarefa finalizada em sala de aula. Obrigado aos envolvidos

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write(@"--- Etanol ou Gasolina? ---
+
+Digite o preço do etanol (R$).....: ");
+double Etanol = Convert.ToDouble (Console.ReadLine());
+
+Console.Write("Digite o preço da gasolina (R$)...: ");
+double Gases = Convert.ToDouble (Console.ReadLine());
+
+double porcentagem = (Etanol / Gases) * 100;
+
+Console.WriteLine($"O preço do etanol corresponde a {porcentagem}% do preço da gasolina");
+
+if (porcentagem >= 70)
+{
+    Console.WriteLine("Recomendação: Abasteça com gasolina");
+}
+
+else
+{
+    Console.WriteLine("Recomendação: Abasteça com etanol");
+}
